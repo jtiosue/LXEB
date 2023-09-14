351291d8400fdc0f0ac418da8a904df4658daaf7
jtiosue <jtiosue@gmail.com>
2023-09-14 01:10:54 -0400

    plot error bars

--- Python/numerical_approx_fourth_moment.py
+++ Python/numerical_approx_fourth_moment.py
@@ -8,9 +8,9 @@ ks = range(1, 10)
 output_filename = "numerical_approx_fourth_moment.csv"
 
 
-def write_to_file(n, k, iters, avg):
+def write_to_file(n, k, iters, avg, std):
 	with open(output_filename, "a") as f:
-		print(f"{n},{k},{iters},{avg}", file=f)
+		print(f"{n},{k},{iters},{avg},{std}", file=f)
 
 
 
@@ -22,16 +22,16 @@ def random_gaussian_matrix(rows, cols):
 
 
 
-write_to_file("n", "k", "iters", "avg")
+write_to_file("n", "k", "iters", "avg", "std")
 
 for n in ns:
 	for k in ks:
 		print(f"Starting n = {n}, k = {k}")
-		s = 0
+		samples = []
 		for _ in range(iters):
 			X = random_gaussian_matrix(k, 2*n)
-			s += np.abs(haf(X.T @ X)) ** 4
+			samples.append(np.abs(haf(X.T @ X)) ** 4)
 
-		write_to_file(n, k, iters, s / iters)
+		write_to_file(n, k, iters, np.mean(samples), np.std(samples))
 
 
--- Python/plot.py
+++ Python/plot.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 from scipy.special import factorial2
 import csv
 import matplotlib.colors as mcolors
+from matplotlib import rc
+import numpy as np
+
+plt.rcParams.update(plt.rcParamsDefault) 
+plt.rcParams['text.usetex'] = True
+plt.rcParams.update({'axes.titlesize':25, 'axes.labelsize':25, 'legend.fontsize':18,'xtick.labelsize':18,'ytick.labelsize':18})
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsfonts}'
 
 COLORS = list(mcolors.TABLEAU_COLORS.keys())
 
@@ -12,8 +21,8 @@ ns = set()
 with open("numerical_approx_fourth_moment.csv") as f:
     for row in csv.DictReader(f, delimiter=','):
     	numerical_data[(int(row['n']), int(row['k']))] = (
-    		# int(row['iters']), float(row['mean']), float(row['std'])
-    		int(row['iters']), float(row['avg'])
+    		int(row['iters']), float(row['avg']), float(row['std'])
+    		# int(row['iters']), float(row['avg'])
     	)
     	ns.add(int(row['n']))
 
@@ -28,26 +37,30 @@ def compute(n, k):
 	return factorial2(2*n-1, exact=True) * sum(g[n][i] * k**i for i in range(len(g[n])))
 
 f = plt.figure()
-plt.xlabel('k')
-plt.ylabel('Haf[X^T X]^4')
-plt.title("Numerical approx vs computed values")
+plt.xlabel(r'$k$')
+plt.ylabel(r'$\mathbb{E}_X \textrm{Haf}[X^T X]^4$')
+plt.title("Numerical Approximation vs. Computed Values for Second Moment")
 
 
 for i, n in enumerate(ns):
 	ks = sorted(set(x[1] for x in numerical_data if x[0] == n))
 
-	plt.semilogy(
+	plt.plot(
 		ks, [compute(n, k) for k in ks], 
 		"-", color=COLORS[i],
-		label=f"n = {n}; computed"
+		label=r"$n = %d$; computed" % n
 	)
-	plt.semilogy(
+	plt.errorbar(
 		ks, [numerical_data[n, k][1] for k in ks], 
-		":", color=COLORS[i],
-		label=f"n = {n}; numerical ({numerical_data[n, 1][0]} iters)"
+		[numerical_data[n, k][2] / np.sqrt(numerical_data[n, 1][0]) for k in ks],
+		# [numerical_data[n, k][2] for k in ks],
+		linestyle=":", color=COLORS[i],
+		label=r"$n = %d$; numerical ($%d$ iters)" % (n, numerical_data[n, 1][0])
 	)
 
-plt.legend()
+plt.legend(fontsize=14)
+
+plt.yscale("log")
 
 f.set_size_inches(18.5, 10.5)
 f.savefig("plot.pdf")
